bec603b35ea565d83f6c8b3f82b53cb41da79d09
akanksha6392 <akankshasingh6392@gmail.com>
2026-02-06 16:13:01 +0000

    Initial commit JS basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="akankshasingh6393@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// accountId = 2// not allowed
+
+accountEmail = "hc2hc@gmail.com"
+accountPassword = "67890"
+accountCity = "Noida"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
