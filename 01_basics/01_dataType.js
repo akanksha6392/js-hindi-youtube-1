3fb5da25395e3c21b140e7954540e980ef2f0628
akanksha6392 <akankshasingh6392@gmail.com>
2026-02-06 17:27:02 +0000

    added datatype code

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3+3) // we are using nodejs, not browser
+
+console.log(3
++
+3)// code readability should be high
+console.log("akku") 
+
+
+let name ="akanksha"// string
+let age = 21// number
+let isLoggedIn = false// boolean
+
+
+//number => 2 to power 53
+//bigint
+//string => "" 
+//boolean => true/false
+//null => standalone value
+//undefined => variable not assigned any value
+//symbol => unique
+
+//object
+console.log(typeof age);
+console.log(typeof undefinfed);//undefined is a type and also a value
+console.log(typeof null);//object
