493b43fadf05fae31e929e49693205f96c910e35
akanksha6392 <akankshasingh6392@gmail.com>
2026-02-10 13:12:57 +0000

    Added JS datatype and conversion files

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(bolleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "akanksha" => true
+let someNumber = 33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
