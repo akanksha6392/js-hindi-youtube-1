93d8cac536c6d47cba40e33b5fb252304b92d0e2
akanksha6392 <akankshasingh6392@gmail.com>
2026-02-11 13:42:45 +0000

    Updated conversionOperation.js code

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,27 @@ let someNumber = 33
 let stringNumber =String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// **********operation *
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+
+
+let str1 = "hello"
+let str2 = " Akanksha"
+let str3 = str1 + str2
+console.log(str3);
+// its not good code
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+//its write code
+console.log("1" + (2 + 2));
+console.log((1 + 2) + "2");
+console.log ( (3+4) * 5 % 2);
